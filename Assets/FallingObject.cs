c7ed2288631a4b26ad4572a27b6aaaddf44b2025
Kristian <huskrist@hotmail.com>
2018-02-07 15:53:56 +0100

    La til eksempel på kode, og hvordan det virker paa skjermen

---
+++ Assets/FallingObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObject : MonoBehaviour {
+
+    public SpriteRenderer sr;
+    public Transform t;
+    private float incrementor = 0.1f;
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        t.position = new Vector3(t.position.x, t.position.y - incrementor);
+	}
+}
