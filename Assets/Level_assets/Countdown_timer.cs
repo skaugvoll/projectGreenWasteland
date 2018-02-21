ac703418a012fcf3722e7b24444ac6c6126597ed
Kristian <huskrist@hotmail.com>
2018-02-21 13:33:16 +0100

    ting

---
+++ Assets/Level_assets/Countdown_timer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown_timer : MonoBehaviour {
+
+    private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sr.material.SetFloat("_Cutoff", Mathf.InverseLerp(0, Screen.width, Input.mousePosition.x));
+    }
+}
