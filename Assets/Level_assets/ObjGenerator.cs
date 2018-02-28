c8edb448867e88d540f7b97c236ed4266014f52c
Kristian <huskrist@hotmail.com>
2018-02-28 10:52:55 +0100

    Klikk og dra funksjonalitet

--- Assets/Level_assets/ObjGenerator.cs
+++ Assets/Level_assets/ObjGenerator.cs
@@ -34,6 +34,7 @@ public class ObjGenerator : MonoBehaviour {
         // a prefab is need to perform the instantiation
         if (equipPrefab != null)
         {
+            print("Creating object");
             // get a random postion to instantiate the prefab - you can change this to be created at a fied point if desired
             Vector3 position = new Vector3(Random.Range(minX + 0.5f, maxX - 0.5f), Random.Range(minY + 0.5f, maxY - 0.5f));
 
