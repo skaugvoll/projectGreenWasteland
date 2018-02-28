283296f2e2112995f224ca94fd7bfb0b5f19e651
Kristian <huskrist@hotmail.com>
2018-02-28 14:22:07 +0100

    drag and drop velocity

--- Assets/Level_assets/FallingObject.cs
+++ Assets/Level_assets/FallingObject.cs
@@ -6,6 +6,8 @@ public class FallingObject : MonoBehaviour {
 
     public SpriteRenderer sr;
     public Transform t;
+    public Rigidbody2D rigidBody;
+
     private float incrementor = 0.1f;
     private bool mouseDown = false;
 
@@ -15,6 +17,11 @@ public class FallingObject : MonoBehaviour {
     private float maxRot = 1;
 
 
+    private Vector2 prevPos;
+    private Vector2 currentPos;
+    private int maxVelocity = 1;
+
+
     public FallingObject(string type)
     {
         this.type = type;
@@ -22,8 +29,11 @@ public class FallingObject : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        maxVelocity = 4;
+        print(maxVelocity + "");
         sr = GetComponent<SpriteRenderer>();
         t = GetComponent<Transform>();
+        rigidBody = GetComponent<Rigidbody2D>();
 	}
 
     // Update is called once per frame
@@ -32,6 +42,10 @@ public class FallingObject : MonoBehaviour {
         if (!mouseDown)
         {
             t.position = new Vector3(t.position.x, t.position.y - incrementor);
+        }else
+        {
+            prevPos = currentPos;
+            currentPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         }
     }
 
@@ -40,9 +54,28 @@ public class FallingObject : MonoBehaviour {
         mouseDown = true;
     }
 
-    private void OnMouseUp()
-    {
+    private void OnMouseUp() {
+        print(maxVelocity);
         mouseDown = false;
+        Vector2 direction = new Vector2(currentPos.x - prevPos.x, currentPos.y - prevPos.y);
+        if (direction.x == 0 && direction.y == 0)
+        {
+            rigidBody.velocity = new Vector2(0, 0);
+            return;
+        }
+        float modifier;
+        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            modifier = maxVelocity / direction.x;
+        }else
+        {
+            modifier = maxVelocity / direction.y;
+        }
+        modifier = Mathf.Abs(modifier);
+        //        print(modifier);
+        rigidBody.velocity = new Vector2(direction.x * modifier, direction.y * modifier);
+       // print(modifier);
+
     }
 
 
