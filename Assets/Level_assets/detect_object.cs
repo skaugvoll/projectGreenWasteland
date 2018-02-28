db78d4863851d73f064cfae22b737f62d7dccade
Christopher Kvarme <ch.kvarme@gmail.com>
2018-02-28 14:52:37 +0100

    Scorecounter, background

--- Assets/Level_assets/detect_object.cs
+++ Assets/Level_assets/detect_object.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class detect_object : MonoBehaviour {
 
+    
 
 
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == tag)
         {
-            Destroy(col.gameObject);
+            GameObject.Find("Main Camera)").GetComponent<Scorecounter>().updateScore(1);
+                
 
-        //else 
-          //  Destroy(col.gameObject);
+            Destroy(col.gameObject);
+           
+
         }
     }
 }
---
+++ Assets/Scorecounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorecounter : MonoBehaviour
+{
+    
+    private int count;
+    private int score;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        count = 0;
+        score = 0;
+    }
+
+    public void updateScore(int score) {
+        count += score;
+        print("Poengsum elns");
+    }
+
+    
+}
