ec841677a37b981697db680f37fb2af8c2945ebd
Christopher Kvarme <ch.kvarme@gmail.com>
2018-03-14 16:04:23 +0100

    level X + level 6

---
+++ Assets/WaveMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveMovement : MonoBehaviour
+{
+
+
+    public SpriteRenderer sr;
+    public Transform t;
+    private float speed = -0.1f;
+    private float heightChange = 5f;
+    private float maxWidth = 190;
+    private float initXpos;
+    //private float initYpos;
+    // Use this for initialization
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        t = GetComponent<Transform>();
+
+        initXpos = t.position.x;
+        //initYpos = t.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newXpos = t.position.x;
+        float newYpos = t.position.y + speed;
+
+        if (newYpos < maxWidth) {
+            newXpos = initXpos;
+            newYpos = 225;
+            newYpos += Random.Range(-heightChange, heightChange);
+        }
+
+        t.position = new Vector2(newXpos, newYpos);
+    }
+}
