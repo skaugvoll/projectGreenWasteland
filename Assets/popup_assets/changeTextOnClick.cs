c884452c32f55de110ce3ecd0b24403b64fa6efa
Sigve Skaugvoll <skaugvoll@gmail.com>
2018-03-21 17:12:50 +0100

    started working on script for changeing text when clicking on talk bubble

--- Assets/popup_assets/changeTextOnClick.cs
+++ Assets/popup_assets/changeTextOnClick.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class changeTextOnClick : MonoBehaviour {
 
 	// Use this for initialization
+	public Button button;
+
 	void Start () {
-		
+		// button = GetComponent<Button>();
+		// button.onClick.AddListener(ButtonOnClick);
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	// this happens everytime the buttion is clicked
+	public void ButtonOnClick() {
+		Debug.Log("Clicked");
 	}
 }
