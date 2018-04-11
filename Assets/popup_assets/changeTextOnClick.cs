4f4111c9b1ff625b85d4cd9c0ead1b6289e5a00f
S.Skaugvoll <skaugvoll@users.noreply.github.com>
2018-04-11 09:34:23 +0200

    Popups (#13)
    * working on first popup screen
    
    * started working on script for changeing text when clicking on talk bubble
    
    * fakk this, tar det neste onsdag. må få til den knappen
    
    * working on first popup screen
    
    * started working on script for changeing text when clicking on talk bubble
    
    * fakk this, tar det neste onsdag. må få til den knappen
    
    * tried making the talkbubble clickable. did not work. dont know how to...
    
    * now the popup should be almost done. just need to load it as a overlay over overworld, and hide it after instructions are done

---
+++ Assets/popup_assets/changeTextOnClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class changeTextOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// this happens everytime the buttion is clicked
+	public void ButtonOnClick() {
+		if(GameObject.Find("msg_I_2").GetComponent<Text>().enabled == true){
+			// SceneManager.LoadScene(scene);
+			Debug.Log("Should now close this popup and display overworld");
+		}
+		else {
+			GameObject.Find("msg_I_1").GetComponent<Text>().enabled = false;
+			GameObject.Find("msg_I_2").GetComponent<Text>().enabled = true;
+		}
+	}
+
+}
